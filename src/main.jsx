0dddecb4517bdaaf46ca7e328e9372ca6e3b9a1b
Anna Pleshakova <ann.pleshakova@gmail.com>
2026-04-16 20:25:16 -0400

    chore: remove sentry debug logs and test captureMessage

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,12 @@ import App from "./App";
 import "./index.css";
 import * as Sentry from "@sentry/react";
 
-console.log("SENTRY DSN:", import.meta.env.VITE_SENTRY_DSN);
-console.log("MODE:", import.meta.env.MODE);
-
 if (import.meta.env.VITE_SENTRY_DSN) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     environment: import.meta.env.MODE,
     tracesSampleRate: 1.0,
   });
-
-  // временно — удалить после теста
-  setTimeout(() => {
-    Sentry.captureMessage("test from prod");
-  }, 2000);
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
